7e3f14d2d5da1c889bc54936c0fba2dde1618cc4
fanbumbot <fanbumbot01@mail.ru>
2026-01-14 12:33:17 +0500

    fix env KNOT_SOCKET and add knot start to entrypoint

---
+++ check_knot.py
@@ -0,0 +1,32 @@
+
+import os
+import sys
+import libknot
+from libknot.control import KnotCtl
+
+# Путь к сокету, который мы пробросили через Volume
+#libknot.Knot("/usr/lib/x86_64-linux-gnu/libknot.so.15")
+socket_path = os.environ.get("KNOT_SOCKET", "/app/storage/sock/knot.sock")
+
+def test_connection():
+    ctl = KnotCtl()
+    try:
+        # 1. Пытаемся подключиться к сокету
+        ctl.connect(socket_path)
+        
+        # 2. Отправляем простую команду статуса
+        # Команда status возвращает блок данных
+        #ctl.send_block(cmd="status")
+        #resp = ctl.receive_block()
+        
+        print("✅ Соединение успешно!")
+        #print(f"Ответ сервера: {resp}")
+        
+    except Exception as e:
+        print(f"❌ Ошибка подключения: {e}")
+        sys.exit(1)
+    finally:
+        ctl.close()
+
+if __name__ == "__main__":
+    test_connection()
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+knotd -C /app/storage/database/ -s /app/storage/sock/knot.sock -d
+
 sleep infinity

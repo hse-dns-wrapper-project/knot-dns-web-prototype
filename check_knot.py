558b054853eb91780d18d493f9027d586e5fb7ee
fanbumbot <fanbumbot01@mail.ru>
2026-01-14 14:27:03 +0500

    update check_knot

--- check_knot.py
+++ check_knot.py
@@ -4,24 +4,14 @@ import sys
 import libknot
 from libknot.control import KnotCtl
 
-# Путь к сокету, который мы пробросили через Volume
-#libknot.Knot("/usr/lib/x86_64-linux-gnu/libknot.so.15")
 socket_path = os.environ.get("KNOT_SOCKET", "/root/knot/knot.sock")
 print(socket_path)
 
 def test_connection():
     ctl = KnotCtl()
     try:
-        # 1. Пытаемся подключиться к сокету
         ctl.connect(socket_path)
-        
-        # 2. Отправляем простую команду статуса
-        # Команда status возвращает блок данных
-        #ctl.send_block(cmd="status")
-        #resp = ctl.receive_block()
-        
         print("✅ Соединение успешно!")
-        #print(f"Ответ сервера: {resp}")
         
     except Exception as e:
         print(f"❌ Ошибка подключения: {e}")
